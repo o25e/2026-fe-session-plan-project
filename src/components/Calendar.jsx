61222695288f47e6ed3f10ec7a3d8f70a0275e11
parkseoyeon9163@gmail.com <162225988+o25e@users.noreply.github.com>
2026-06-19 15:49:44 +0900

    fix: 날짜 저장 방식 UTC → 로컬로 변경

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -7,7 +7,13 @@ function Calendar({selectedDate, setSelectedDate, todos}) {
     const year = now.getFullYear();
     const month = now.getMonth();
 
-    const formatDate = (date) => date.toISOString().split("T")[0];
+    const formatDate = (date) => {
+        const year = date.getFullYear();
+        const month = String(date.getMonth() + 1).padStart(2, "0");
+        const day = String(date.getDate()).padStart(2, "0");
+
+        return `${year}-${month}-${day}`;
+    };
 
     const today = new Date();
 
--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -10,7 +10,13 @@ function Main() {
     const [editingId, setEditingId] = useState(null);
     const [editText, setEditText] = useState("");
 
-    const formDate = (date) => date.toISOString().split("T")[0];
+    const formDate = (date) => {
+        const year = date.getFullYear();
+        const month = String(date.getMonth() + 1).padStart(2, "0");
+        const day = String(date.getDate()).padStart(2, "0");
+
+        return `${year}-${month}-${day}`;
+    };
 
     const formatTitleDate = (date) => {
         return `${date.getMonth() + 1}월 ${date.getDate()}일`;
